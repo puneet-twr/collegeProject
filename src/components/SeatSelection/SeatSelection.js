6a6a0457297f0e6b3288e418e7968c7ed195aeaa
Puneet <pt.twr2@gmail.com>
2022-10-26 14:27:11 +0530

    another commit

--- src/components/SeatSelection/SeatSelection.js
+++ src/components/SeatSelection/SeatSelection.js
@@ -1,10 +1,123 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './SeatSelection.css';
 
 function SeatSelection() {
-    
+//   const container = document.querySelector('.container');
+// const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+// const count = document.getElementById('count');
+// const total = document.getElementById('total');
+// const movieSelect = document.getElementById('movie');
+
+// let ticketPrice = +movieSelect.value;
+
+//Update total and count
+// function updateSelectedCount() {
+//   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+//   const selectedSeatsCount = selectedSeats.length;
+//   count.innerText = selectedSeatsCount;
+//   total.innerText = selectedSeatsCount * ticketPrice;
+// }
+const handleSeatClick=(e)=>{
+  if (e.target.classList.contains('seat') &&
+     !e.target.classList.contains('occupied')) {
+    e.target.classList.toggle('selected');
+    console.log(e.target);
+  }
+}
   return (
-    <div>SeatSelection</div>
+    <div class="movie-container" >
+    <label>Pick a movie: </label>
+    <select id="movie" >
+      <option value="250">Interstellar (Rs. 250)</option>
+      <option value="200">Kabir Singh (Rs. 200)</option>
+      <option value="150">Duniyadari (Rs. 150)</option>
+      <option value="100">Natsamrat (Rs. 100)</option>
+    </select>
+    
+    <ul class="showcase">
+      <li>
+        <div class="seat"></div>
+        <small>N/A</small>
+      </li>
+      <li>
+        <div class="seat selected"></div>
+        <small>Selected</small>
+      </li>
+      <li>
+        <div class="seat occupied"></div>
+        <small>Occupied</small>
+      </li>    
+    </ul>
+    
+    <div class="container" onClick={handleSeatClick}>
+      <div class="screen"></div>
+      
+      <div class="row">
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+        </div>
+        <div class="row">
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat occupied"></div>
+          <div class="seat occupied"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+        </div>
+        <div class="row">
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat occupied"></div>
+          <div class="seat occupied"></div>
+        </div>
+        <div class="row">
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+        </div>
+        <div class="row">
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat occupied"></div>
+          <div class="seat occupied"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+        </div>
+        <div class="row">
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat"></div>
+          <div class="seat occupied"></div>
+          <div class="seat occupied"></div>
+          <div class="seat occupied"></div>
+          <div class="seat"></div>
+        </div>
+      
+      <p class="text">
+        You have selected <span id="count">0</span> seats for the total price of Rs. <span id="total">0</span>
+      </p>
+    </div>
+  </div>
   )
 }
 
